8151789412c3c151aff7a43d4547168a14e2ca64
jjwumaster <jjwumaster@gmail.com>
2017-11-03 16:11:57 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |item|
+      item.split("").sort == word.split("").sort
+    end
+    # It should return all matches in an array.
+    # If no matches exist, it should return an empty array.
+  end
+
+end
