0603260a2c9d7aa2a83247af6545d2d6267a3968
George Gore <georgelgore@gmail.com>
2017-11-03 15:56:34 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(phrase)
+    phrase.select{|string| @word.chars.sort == string.chars.sort }
+  end
+
+end
