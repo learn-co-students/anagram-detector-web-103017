20dcfac904e78dc3a82501042d20805f1ec913b6
eshim12 <ellishim@gmail.com>
2017-11-04 00:38:12 -0400

    Complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    ltrs_arr = @word.split("").sort
+
+    array.select do |word|
+      if word.split("").sort == ltrs_arr
+        word
+      end
+    end
+  end
+
+end
