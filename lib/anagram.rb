45ffeda42b63e25e648c4952e11be4ccb92423f7
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-05 22:18:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(phrase)
+    match_case = @word.downcase.split('').sort
+    anagram = phrase.collect{|x| x if x.downcase.split('').sort==match_case}.compact
+    return anagram
+  end
+
+end
