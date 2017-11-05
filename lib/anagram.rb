5b6f68eb07ece1de78a1b2187ca4cb3c9f777741
RochelLevi <rochlevi@gmail.com>
2017-11-05 13:01:35 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_arr)
+    words_arr.select{|w| w.chars.sort == word.chars.sort}
+  end
+
+end
