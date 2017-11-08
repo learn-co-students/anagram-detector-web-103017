c9be325a1356f4873083f596475fba643b97be2f
dansbands <dansbands@hotmail.com>
2017-11-08 18:09:46 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+      @word = word
+  end
+
+  def match(array)
+    array.select do |item|
+      item.chars.sort == word.chars.sort
+    end
+  end
+end
