7526f51cb99fc8d439cc8c743288b2731d2b469e
github username <github email address>
2017-11-08 16:49:59 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    # Break up each word into an array
+    # Order the array
+    # see if the arrays match
+    # if they do, send back the word
+    home = @word.split('').sort
+
+    result = array.map do |word|
+      if word.split('').sort == home
+        word
+      end
+    end
+    result.compact
+
+  end
+end
